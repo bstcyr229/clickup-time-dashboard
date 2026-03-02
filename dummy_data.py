8abcc1ed27ea056212b1d0956471fb4485229475
Bryan St. Cyr <bstcyr229@gmail.com>
2026-03-02 15:44:45 -0700

    working on nested loop for dummy data

--- dummy_data.py
+++ dummy_data.py
@@ -21,44 +21,50 @@ def generate_dummy_data():
     "Khalid": 100,
     "Eva": 110,
     "Pierre": 75
-}
-    days_of_week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
-    work_days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+}   
     rows = []
+    work_days = pd.Categorical(["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"])
+    df['work_days'] = pd.Categorical(df['day'], categories=work_days, ordered=True)   
     
     estimated = round(random.uniform(1,12),1)
     actual = round(estimated * random.uniform(0.5, 2.0),1)
     #estimated = int(estimated)
     #actual = int(actual) 
 
+    
 
-    for _ in range(60):
-        estimated = round(random.uniform(1,12),1)
-        actual = round(estimated * random.uniform(0.5, 2.0),1)
-        selected_assignee = random.choice(staff)
-        team_register = teams[selected_assignee]
-        task_id = uuid.uuid4()
-        task_id = str(task_id)
 
-        rows.append({
-            "assignee": selected_assignee,
-            "project":random.choice(projects),
-            "task":random.choice(tasks),
-            "task_id": task_id[0:5],
-            "estimated_hours":estimated,
-            "actual_hours":actual,
-            "variance": round(actual - estimated,1),
-            "Team": team_register, 
-            "hourly_rate": rates[selected_assignee],
-            "total_cost": round(actual * rates[selected_assignee],2),
-            "day": random.choice(work_days),
+    for _ in range (4):
+        for day_name in work_days:
+            for team_name in teams:
+        
+                estimated = round(random.uniform(1,12),1)
+                actual = round(estimated * random.uniform(1, 12),1)
+                selected_assignee = random.choice(staff)
+                team_register = teams[selected_assignee]
+                task_id = uuid.uuid4()
+                task_id = str(task_id)
+                    
+                rows.append({
+                    "assignee": random.choice(teams[team_name]),
+                    "project":random.choice(projects),
+                    "task":random.choice(tasks),
+                    "task_id": task_id[0:5],
+                    "estimated_hours":estimated,
+                    "actual_hours":actual,
+                    "variance": round(actual - estimated,1),
+                    "Team": team_register, 
+                    "hourly_rate": rates[selected_assignee],
+                    "total_cost": round(actual * rates[selected_assignee],2),
+                    "day": day_name,
 
             
 
         })
 
-    
-    return pd.DataFrame(rows)
+    df = pd.DataFrame(rows)
+
+    return df
     
 if __name__ == "__main__":
     df= generate_dummy_data()
