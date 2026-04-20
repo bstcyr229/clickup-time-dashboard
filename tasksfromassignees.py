2ee11e79cddfef89d2cd79f64a2aea3eb7418d8d
Bryan St. Cyr <bstcyr229@gmail.com>
2026-04-20 11:42:58 -0600

    Added basic error handling to the fetching tasks function, will continue on the empty entries problem next session.

--- tasksfromassignees.py
+++ tasksfromassignees.py
@@ -1,3 +1,6 @@
+#TO ADD- Error Handling for API calls 
+
+
 
 import dummy_data as gdd
 import pandas as pd
@@ -28,16 +31,21 @@ def fetching_tasks():
 
 
     # This line will get all of the tasks in your ws, I am just configuring with a test space get_tasks = requests.get(f"https://api.clickup.com/api/v2/team/{workspace_id}/task", headers=headers")
-    get_tasks= requests.get(f'https://api.clickup.com/api/v2/team/{workspace_id}/task?space_ids[]={test_space_id}',headers=headers) 
-    get_tasks_json = get_tasks.json().get("tasks")
-    due_date = (dt(2026,4 ,19).timestamp() * unix_converter) 
-    start_date = (dt(2026,4,12).timestamp() * unix_converter) 
-    
-    get_entries_from_before_due_and_start_dates = requests.get(f"https://api.clickup.com/api/v2/team/{workspace_id}/time_entries?start_date={start_date}&end_date={due_date}", headers=headers)
-    date_filtered_entries = get_entries_from_before_due_and_start_dates.json().get("entries")
-    print(date_filtered_entries)
-
-    # get_entries = requests.get(f"")
+    get_tasks_request= requests.get(f'https://api.clickup.com/api/v2/team/{workspace_id}/task?space_ids[]={test_space_id}',headers=headers) 
+    if get_tasks_request.status_code != 200:
+        print(f"Task request API call failed. ERROR CODE: { get_tasks_request}")
+    else:
+        get_tasks_json = get_tasks_request.json().get("tasks")
+        due_date = (dt(2026,4 ,19).timestamp() * unix_converter) 
+        start_date = (dt(2026,4,12).timestamp() * unix_converter) 
+        
+    get_entries_from_before_due_and_start_dates = requests.get(f"https://api.clickup.com/api/v2/team/{workspace_id}/time_entries?end_date_lt={due_date}", headers=headers)
+    if get_entries_from_before_due_and_start_dates.status_code != 200:
+        print(f"Date filtered entries request API call failed, ERROR CODE: {get_entries_from_before_due_and_start_dates}")
+    else:
+        date_filtered_entries = get_entries_from_before_due_and_start_dates.json().get("entries")
+        if date_filtered_entries is None:
+            print("No entries found")
 
 def aggregrate_task_data():
     pass
