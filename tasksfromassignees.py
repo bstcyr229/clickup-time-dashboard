73c4add0363531f2ccb13fed822140f224844c8f
Bryan St. Cyr <bstcyr229@gmail.com>
2026-04-21 17:50:38 -0600

    Fixed entry fixing error, it was an error on my get method, I put entries instead of data.

--- tasksfromassignees.py
+++ tasksfromassignees.py
@@ -1,4 +1,4 @@
-#TO ADD- Error Handling for API calls 
+#TO ADD- Have fetch tasks pass data into aggregate_task_data  
 
 
 
@@ -9,8 +9,7 @@ import altair as alt
 import requests
 import os
 import json 
-from datetime import datetime as dt 
-from datetime import timedelta
+from datetime import datetime as dt, timedelta , timezone 
 
 from dotenv import load_dotenv
 load_dotenv()
@@ -20,13 +19,15 @@ def main():
     pass 
 def fetching_tasks():
     click_up_api_key = os.getenv("cu_api_key")
-    headers = {"Authorization": click_up_api_key}
+    headers =   {"Authorization": click_up_api_key, 
+                    "accept": "application/json",
+                    "Content-Type": "application/json"}
 
     workspace_id = os.getenv("workspace_id")
     test_space_id = os.getenv("test_space")
-
-
-    milisecond_converter = 3600000
+    start_date = dt(2026, 4, 1, tzinfo=timezone.utc)
+    end_date = dt(2026, 4, 21, tzinfo=timezone.utc)
+    
     unix_converter = 1000
 
 
@@ -36,16 +37,19 @@ def fetching_tasks():
         print(f"Task request API call failed. ERROR CODE: { get_tasks_request}")
     else:
         get_tasks_json = get_tasks_request.json().get("tasks")
-        end_date = int(dt(2026,4 ,19).timestamp() * unix_converter) 
-        start_date = int(dt(2026,4,12).timestamp() * unix_converter) 
-    get_entries_from_before_due_and_start_dates = requests.get(f'https://api.clickup.com/api/v2/team/{workspace_id}/time_entries?start_date={start_date}&end_date={end_date}', headers=headers)
-    print(get_entries_from_before_due_and_start_dates)
+    
+    start_date_ms = int(start_date.timestamp() * unix_converter)
+    end_date_ms = int(end_date.timestamp() * unix_converter)    
+    get_entries_from_before_due_and_start_dates = requests.get(f'https://api.clickup.com/api/v2/team/{workspace_id}/time_entries?start_date={start_date_ms}&end_date={end_date_ms}', headers=headers)
+    print(get_entries_from_before_due_and_start_dates.json().keys())
     if get_entries_from_before_due_and_start_dates.status_code != 200:
         print(f"Date filtered entries request API call failed, ERROR CODE: {get_entries_from_before_due_and_start_dates}")
     else:
-        date_filtered_entries = get_entries_from_before_due_and_start_dates.json().get("entries")
+        date_filtered_entries = get_entries_from_before_due_and_start_dates.json().get("data")
         if date_filtered_entries is None:
             print("No entries found")
+        else:
+            print(date_filtered_entries)
             
 def aggregrate_task_data():
     pass
